7b690643bbf797f40d10568d5cd5d0a07f7fc989
Aron Galdon <41184401+arongaldon@users.noreply.github.com>
2018-07-20 09:41:48 +0000

    Added exercise of writing integers to a file

--- cppStroustrupExercises/X0509/main.cpp
+++ cppStroustrupExercises/X0509/main.cpp
@@ -1,9 +1,20 @@
 /* -*- c++ -*- */
 
 #include <iostream>
+#include <fstream>
 
+using namespace std;
+
+/**
+ * Escribir un centenar de enteros en un fichero
+ */
 int main()
 {
-    std::cout << "Hello, world!\n";
-    return 0;
+	ofstream is{ "integers" }; // Output stream for file "integers"
+
+	for (int x = 1; x < 100; ++x) {
+		is << x << endl;
+	} // Print vector
+	
+    return EXIT_SUCCESS;
 }
