c7185f3e65be9e32e08db17ec37fd7c7f020fe90
Aron Galdon Gines <41184401+arongaldon@users.noreply.github.com>
2019-04-30 13:58:48 +0200

    Fix versions

--- baseball/MakefileAMano/baseballConfig.h
+++ baseball/MakefileAMano/baseballConfig.h
@@ -1,3 +1,3 @@
 // the configured options and settings for baseball
-#define baseball_VERSION_MAJOR "MakefileAMano"
-#define baseball_VERSION_MINOR "MakefileAMano"
+#define baseball_VERSION_MAJOR 1
+#define baseball_VERSION_MINOR 1
